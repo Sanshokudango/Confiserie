f80d1ac2157851695cedd896ff6f7efe648f0dfb
Hiro11190422 <hiroshi01.toyoshima@edu.pref.shizuoka.jp>
2016-01-28 22:23:14 +0900

    file to uploade 201601282222

---
+++ js/accordion.js
@@ -0,0 +1,12 @@
+﻿$(function(){
+$("ul.submenu").hide();
+$("div.category").click(function(){
+$("ul.submenu li").click(function(){
+$("ul.submenu").hide();
+});
+$("ul.submenu").slideUp();
+if($("+ul",this).css("display")=="none"){
+$("+ul",this).slideDown();
+}
+});
+});
---
+++ js/map.js
@@ -0,0 +1,50 @@
+﻿//<![CDATA[
+
+var map, httpObj;
+
+function initialize()  {
+	map = new GMap2(document.getElementById("map"));
+	map.setCenter(new GLatLng(34.834752, 138.183324), 13);
+	map.addControl(new GLargeMapControl());
+	var msec = (new Date()).getTime();
+	httpObj = GXmlHttp.create();
+	httpObj.open("get", "./data/mise.csv?cache="+msec);
+
+	httpObj.onreadystatechange = function() {
+		if ((httpObj.readyState == 4) && (httpObj.status == 200)) addMarker();
+	}
+	httpObj.send(null);
+}
+
+function addMarker() {
+	var CR = String.fromCharCode(13);
+	var txt = httpObj.responseText;
+	var pointData = txt.split(CR);
+		for (var i=0; i<pointData.length; i++) {
+			var pnt = pointData[i].split(",");
+			var html = "<a href="+pnt[13]+","+pnt[14]+">"+pnt[0]+"</a>";
+			var point = new GLatLng(pnt[11],pnt[12]);
+			/*var icon=new GIcon();
+			
+				
+					icon.image="./images/a.png";
+				
+			icon.iconSize=new GSize(20, 20);
+			icon.iconAnchor = new GPoint(10, 10);
+			*/
+			var marker = createMarker(point,html);
+
+			
+		}
+}
+function createMarker(point,html){
+	var marker = new GMarker(point);
+	map.addOverlay(marker);
+	GEvent.addListener(marker, "click", function() {
+   		marker.openInfoWindowHtml(html);
+ 	 });
+	return marker;
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+//]]>
